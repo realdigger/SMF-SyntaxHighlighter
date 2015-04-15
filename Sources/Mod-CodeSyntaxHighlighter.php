7dc3a725c7af71f7085fdf57f17c8b414fb24fd9
realdigger <neo@samtel.ru>
2015-04-15 07:53:28 +0400

    some fixes

--- Sources/Mod-CodeSyntaxHighlighter.php
+++ Sources/Mod-CodeSyntaxHighlighter.php
@@ -25,7 +25,6 @@ function loadCodeSyntaxHighlighterHooks()
     add_integration_function('integrate_admin_areas', 'addCodeSyntaxHighlighterAdminArea', false);
     add_integration_function('integrate_modify_modifications', 'addCodeSyntaxHighlighterAdminAction', false);
     add_integration_function('integrate_bbc_codes', 'changeCodeSyntaxHighlighterTag', false);
-
     //add_integration_function('integrate_create_post', 'addCodeSyntaxHighlighterForPost', false);
 }
 
@@ -116,53 +115,52 @@ function loadCodeSyntaxHighlighterAssets()
     global $modSettings, $context, $settings;
 
     $context['insert_after_template'] .= '
-        <script src="' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/XRegExp.js" type="text/javascript"></script>
-        <script src="' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/shCore.js" type="text/javascript"></script>
-        <script src="' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/shAutoloader.js" type="text/javascript"></script>
-        <script language="javascript">
-            SyntaxHighlighter.config.bloggerMode = true;
-            SyntaxHighlighter.config.stripBrs = true;
-            ' . (!empty($modSettings['code_syntax_highlighter_toolbar']) ? 'SyntaxHighlighter.defaults["toolbar"] = true;' : 'SyntaxHighlighter.defaults["toolbar"] = false;') . '
-            ' . (!empty($modSettings['code_syntax_highlighter_gutter']) ? 'SyntaxHighlighter.defaults["gutter"] = true;' : 'SyntaxHighlighter.defaults["gutter"] = false;') . '
-            ' . (!empty($modSettings['code_syntax_highlighter_auto_links']) ? 'SyntaxHighlighter.defaults["auto-links"] = true;' : 'SyntaxHighlighter.defaults["auto-links"] = false;') . '
-            ' . (!empty($modSettings['code_syntax_highlighter_pad_line_numbers']) ? 'SyntaxHighlighter.defaults["pad-line-numbers"] = true;' : 'SyntaxHighlighter.defaults["pad-line-numbers"] = false;') . '
-            ' . (!empty($modSettings['code_syntax_highlighter_smart_tabs']) ? 'SyntaxHighlighter.defaults["smart-tabs"] = true;' : 'SyntaxHighlighter.defaults["smart-tabs"] = false;') . '
-            ' . (!empty($modSettings['code_syntax_highlighter_tab_size']) ? 'SyntaxHighlighter.defaults["tab-size"] = ' . $modSettings['code_syntax_highlighter_tab_size'] . ';' : 'SyntaxHighlighter.defaults["tab-size"] = 4;') . '
-            SyntaxHighlighter.autoloader(
-	            "applescript			' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushAppleScript.js",
-	            "actionscript3 as3		' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushAS3.js",
-                "bash shell				' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushBash.js",
-	            "coldfusion cf			' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushColdFusion.js",
-                "cpp c					' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushCpp.js",
-	            "c# c-sharp csharp		' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushCSharp.js",
-	            "css					' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushCss.js",
-	            "delphi pascal			' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushDelphi.js",
-	            "diff patch pas			' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushDiff.js",
-	            "erl erlang				' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushErlang.js",
-	            "groovy					' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushGroovy.js",
-	            "haxe hx				' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushHaxe.js",
-	            "java					' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushJava.js",
-	            "jfx javafx				' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushJavaFX.js",
-                "js jscript javascript	' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushJScript.js",
-	            "perl pl				' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushPerl.js",
-	            "php					' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushPhp.js",
-	            "text plain				' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushPlain.js",
-	            "py python				' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushPython.js",
-	            "ruby rails ror rb		' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushRuby.js",
-	            "scala					' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushScala.js",
-	            "sql					' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushSql.js",
-	            "vb vbnet				' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushVb.js",
-	            "xml xhtml xslt html	' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushXml.js"
-            );
-            SyntaxHighlighter.all();
-	    </script>
-	    ';
+                <script src="' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/XRegExp.js" type="text/javascript"></script>
+                <script src="' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/shCore.js" type="text/javascript"></script>
+                <script src="' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/shAutoloader.js" type="text/javascript"></script>
+                <script type="text/javascript"><!-- // --><![CDATA[
+                    SyntaxHighlighter.config.bloggerMode = true;
+                    SyntaxHighlighter.config.stripBrs = true;
+                    ' . (!empty($modSettings['code_syntax_highlighter_toolbar']) ? 'SyntaxHighlighter.defaults["toolbar"] = true;' : 'SyntaxHighlighter.defaults["toolbar"] = false;') . '
+                    ' . (!empty($modSettings['code_syntax_highlighter_gutter']) ? 'SyntaxHighlighter.defaults["gutter"] = true;' : 'SyntaxHighlighter.defaults["gutter"] = false;') . '
+                    ' . (!empty($modSettings['code_syntax_highlighter_auto_links']) ? 'SyntaxHighlighter.defaults["auto-links"] = true;' : 'SyntaxHighlighter.defaults["auto-links"] = false;') . '
+                    ' . (!empty($modSettings['code_syntax_highlighter_pad_line_numbers']) ? 'SyntaxHighlighter.defaults["pad-line-numbers"] = true;' : 'SyntaxHighlighter.defaults["pad-line-numbers"] = false;') . '
+                    ' . (!empty($modSettings['code_syntax_highlighter_smart_tabs']) ? 'SyntaxHighlighter.defaults["smart-tabs"] = true;' : 'SyntaxHighlighter.defaults["smart-tabs"] = false;') . '
+                    ' . (!empty($modSettings['code_syntax_highlighter_tab_size']) ? 'SyntaxHighlighter.defaults["tab-size"] = ' . $modSettings['code_syntax_highlighter_tab_size'] . ';' : 'SyntaxHighlighter.defaults["tab-size"] = 4;') . '
+                    SyntaxHighlighter.autoloader(
+	                    "applescript			' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushAppleScript.js",
+	                    "actionscript3 as3		' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushAS3.js",
+                        "bash shell				' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushBash.js",
+	                    "coldfusion cf			' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushColdFusion.js",
+                        "cpp c					' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushCpp.js",
+	                    "c# c-sharp csharp		' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushCSharp.js",
+	                    "css					' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushCss.js",
+	                    "delphi pascal			' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushDelphi.js",
+	                    "diff patch pas			' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushDiff.js",
+	                    "erl erlang				' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushErlang.js",
+	                    "groovy					' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushGroovy.js",
+	                    "haxe hx				' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushHaxe.js",
+	                    "java					' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushJava.js",
+	                    "jfx javafx				' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushJavaFX.js",
+                        "js jscript javascript	' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushJScript.js",
+	                    "perl pl				' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushPerl.js",
+	                    "php					' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushPhp.js",
+	                    "text plain				' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushPlain.js",
+	                    "py python				' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushPython.js",
+	                    "ruby rails ror rb		' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushRuby.js",
+	                    "scala					' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushScala.js",
+	                    "sql					' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushSql.js",
+	                    "vb vbnet				' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushVb.js",
+	                    "xml xhtml xslt html	' . $settings['default_theme_url'] . '/scripts/CodeSyntaxHighlighter/' . 'shBrushXml.js"
+                    );
+                    SyntaxHighlighter.all();
+	            // ]]></script>
+    ';
 
     $context['html_headers'] .= '
-        <link href="' . $settings['default_theme_url'] . '/css/CodeSyntaxHighlighter/' . 'shCore.css" rel="stylesheet" type="text/css" />
-        <link href="' . $settings['default_theme_url'] . '/css/CodeSyntaxHighlighter/' . $modSettings['code_syntax_highlighter_theme'] . '.css" rel="stylesheet" type="text/css" />
-        ';
-
+    <link rel="stylesheet" type="text/css" href="' . $settings['default_theme_url'] . '/css/CodeSyntaxHighlighter/' . 'shCore.css" />
+    <link rel="stylesheet" type="text/css" href="' . $settings['default_theme_url'] . '/css/CodeSyntaxHighlighter/' . $modSettings['code_syntax_highlighter_theme'] . '.css" />
+    ';
 }
 
 
@@ -172,7 +170,7 @@ function loadCodeSyntaxHighlighterAssets()
  */
 function changeCodeSyntaxHighlighterTag(&$codes = array())
 {
-    global $cache_id, $txt;
+    global $txt;
 
     foreach ($codes as $codeId => $code) {
         if ($code['tag'] == 'code' && $code['type'] == 'unparsed_equals_content') {
@@ -184,7 +182,7 @@ function changeCodeSyntaxHighlighterTag(&$codes = array())
                 'content' => '<div class="codeheader">' . $txt['code'] . ': ($2) </div><pre name="code" class="brush: $2">$1</pre>',
                 'validate' => create_function('&$tag, &$data, $disabled', '
                     if (!isset($disabled[\'code\']))
-					    $data[0] = parse_bbc($data[0], false, \'' . $cache_id . '\');
+					    $data[0] = rtrim($data[0], "\n\r");
             	'),
                 'block_level' => true,
                 'disabled_content' => '<pre>$1</pre>',
@@ -198,7 +196,7 @@ function changeCodeSyntaxHighlighterTag(&$codes = array())
                 'content' => '<div class="codeheader">' . $txt['code'] . ': (text) </div><pre name="code" class="brush: php">$1</pre>',
                 'validate' => create_function('&$tag, &$data, $disabled', '
                     if (!isset($disabled[\'code\']))
-					    $data = parse_bbc($data, false, \'' . $cache_id . '\');
+					    $data = rtrim($data[0], "\n\r");
             	'),
                 'block_level' => true,
                 'disabled_content' => '<pre>$1</pre>',
@@ -212,7 +210,7 @@ function changeCodeSyntaxHighlighterTag(&$codes = array())
                 'content' => '<div class="codeheader">' . $txt['code'] . ': (php) </div><pre name="code" class="brush: php">$1</pre>',
                 'validate' => create_function('&$tag, &$data, $disabled', '
                     if (!isset($disabled[\'php\']))
-					    $data = parse_bbc($data, false, \'' . $cache_id . '\');
+					    $data = rtrim($data[0], "\n\r");
             	'),
                 'block_level' => true,
                 'disabled_content' => '<pre>$1</pre>',
