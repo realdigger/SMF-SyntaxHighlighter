ab4076dcfa53cb6bcca525d73fedd1aa62ce6c42
realdigger <digger@mysmf.net>
2018-02-01 10:29:25 +0400

    Update languages.

---
+++ Themes/default/languages/CodeSyntaxHighlighter/CodeSyntaxHighlighter.english.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @package SMF Code Syntax Highlighter Mod
+ * @author digger http://mysmf.net
+ * @copyright 2015
+ * @license MIT http://opensource.org/licenses/mit-license.php
+ * @version 2.0
+ */
+
+$txt['code_syntax_highlighter_title_menu'] = 'Code Syntax Highlighter';
+$txt['code_syntax_highlighter_title_settings'] = 'Settings';
+$txt['code_syntax_highlighter_smart_tabs'] = 'Turn smart tabs feature on';
+$txt['code_syntax_highlighter_hljs_theme'] = $txt['code_syntax_highlighter_theme'] = 'CSS theme for code blocks';
+$txt['code_syntax_highlighter_hljs_load'] = $txt['code_syntax_highlighter_sh_load'] = 'Load from';
+
+$txt['code_syntax_highlighter_font_size'] = 'Font size';
+$txt['code_syntax_highlighter_font_small'] = 'small';
+$txt['code_syntax_highlighter_font_medium'] = 'medium';
+$txt['code_syntax_highlighter_font_large'] = 'big';
+
+
+$txt['code_syntax_highlighter_engine'] = 'Highlighter engine';
+$txt['code_syntax_highlighter_tab_size'] = 'Adjust tab size';
+
+
+$txt['code_syntax_highlighter_title_syntax_highlighter'] = 'SyntaxHighlighter';
+$txt['code_syntax_highlighter_gutter'] = 'Turn gutter with line numbers on';
+$txt['code_syntax_highlighter_auto_links'] = 'Turn detection of links in the highlighted element on';
+$txt['code_syntax_highlighter_pad_line_numbers'] = 'Automatically pad numbers with minimum required number of leading zeroes';
+$txt['code_syntax_highlighter_smart_tabs'] = 'Turn smart tabs feature on';
+
+$txt['code_syntax_highlighter_title_hljs'] = 'Highlight.js';
---
+++ Themes/default/languages/CodeSyntaxHighlighter/CodeSyntaxHighlighter.russian.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @package SMF Code Syntax Highlighter Mod
+ * @author digger http://mysmf.net
+ * @copyright 2015
+ * @license MIT http://opensource.org/licenses/mit-license.php
+ * @version 2.0
+ */
+
+$txt['code_syntax_highlighter_title_menu'] = 'Подсветка кода';
+$txt['code_syntax_highlighter_title_settings'] = 'Общие настройки';
+$txt['code_syntax_highlighter_engine'] = 'Движок подсветки';
+$txt['code_syntax_highlighter_hljs_theme'] = $txt['code_syntax_highlighter_theme'] = 'Тема оформления для блоков с кодом';
+$txt['code_syntax_highlighter_hljs_load'] = $txt['code_syntax_highlighter_sh_load'] = 'Загружать';
+
+$txt['code_syntax_highlighter_font_size'] = 'Размер шрифта для блоков с кодом';
+$txt['code_syntax_highlighter_font_small'] = 'маленький';
+$txt['code_syntax_highlighter_font_medium'] = 'средний';
+$txt['code_syntax_highlighter_font_large'] = 'крупный';
+
+$txt['code_syntax_highlighter_title_syntax_highlighter'] = 'SyntaxHighlighter';
+$txt['code_syntax_highlighter_gutter'] = 'Показывать номера строк';
+$txt['code_syntax_highlighter_auto_links'] = 'Кликабельные ссылки в коде';
+$txt['code_syntax_highlighter_pad_line_numbers'] = 'Добавлять нули к номерам строк';
+$txt['code_syntax_highlighter_smart_tabs'] = 'Умные табы';
+
+$txt['code_syntax_highlighter_title_hljs'] = 'Highlight.js';
